d7c32fe45f4b35f9e20af9d199d9c259d32d282e
Paddy <paddy@carvers.co>
2017-02-12 06:10:21 -0800

    Initial pass at an API for our RSVPs.
    Initial pass at an API to store our RSVP information on App Engine.

---
+++ rsvp/party.go
@@ -0,0 +1,228 @@
+package rsvp
+
+import (
+	"errors"
+
+	"code.secondbit.org/uuid.hg"
+
+	"golang.org/x/net/context"
+	"google.golang.org/appengine/datastore"
+)
+
+const (
+	partyKind  = "Party"
+	personKind = "Person"
+)
+
+var (
+	ErrMagicWordNotFound = errors.New("magic word not found")
+)
+
+type Party struct {
+	ID        string         `datastore:"-" json:"ID"`
+	Lead      *datastore.Key `json:"-"`
+	LeadID    string         `datastore:"-" json:"lead,omitempty"`
+	Name      string         `json:"name,omitempty"`
+	SortValue string         `json:"sortValue"`
+	NumPeople int            `json:"numPeople,omitempty"`
+	Replied   bool           `json:"replied"`
+	NumComing int            `json:"numComing,omitempty"`
+	Address   string         `json:"address,omitempty"`
+	Email     string         `json:"email,omitempty"`
+	Phone     string         `json:"phone,omitempty"`
+	MagicWord string         `json:"codeWord,omitempty"`
+}
+
+func (p Party) Key(ctx context.Context) *datastore.Key {
+	return datastore.NewKey(ctx, partyKind, p.ID, 0, nil)
+}
+
+func (p Party) FillKeyIDs(ctx context.Context) Party {
+	if p.Lead != nil && p.LeadID == "" {
+		p.LeadID = p.Lead.StringID()
+	}
+	if p.Lead == nil && p.LeadID != "" {
+		p.Lead = datastore.NewKey(ctx, partyKind, p.LeadID, 0, nil)
+	}
+	return p
+}
+
+func CreateParties(ctx context.Context, parties []Party) ([]Party, error) {
+	var keys []*datastore.Key
+	var err error
+	for pos, party := range parties {
+		if party.ID == "" {
+			party.ID = uuid.NewID().String()
+		}
+		party = party.FillKeyIDs(ctx)
+		parties[pos] = party
+		keys = append(keys, party.Key(ctx))
+	}
+	_, err = datastore.PutMulti(ctx, keys, parties)
+	return parties, err
+}
+
+type Person struct {
+	ID                   string         `datastore:"-" json:"ID"`
+	Party                *datastore.Key `json:"-"`
+	PartyID              string         `datastore:"-" json:"party,omitempty"`
+	Name                 string         `json:"name,omitempty"`
+	GetsPlusOne          bool           `json:"getsPlusOne"`
+	PlusOne              *datastore.Key `json:"-"`
+	PlusOneID            string         `datastore:"-" json:"plusOne,omitempty"`
+	IsPlusOne            bool           `json:"isPlusOne"`
+	IsPlusOneOf          *datastore.Key `json:"-"`
+	IsPlusOneOfID        string         `datastore:"-" json:"isPlusOneOf,omitempty"`
+	Replied              bool           `json:"replied"`
+	Reply                bool           `json:"reply"`
+	DietaryRestrictions  string         `json:"dietaryRestrictions,omitempty"`
+	IsChild              bool           `json:"isChild"`
+	WillAccompany        *datastore.Key `json:"-"`
+	WillAccompanyID      string         `datastore:"-" json:"willAccompany,omitempty"`
+	BabysitterForWedding bool           `json:"babysitterForWedding"`
+	BabysitterForEvents  bool           `json:"babysitterForEvents"`
+}
+
+func (p Person) Key(ctx context.Context) *datastore.Key {
+	return datastore.NewKey(ctx, personKind, p.ID, 0, nil)
+}
+
+func (p Person) FillKeyIDs(ctx context.Context) Person {
+	if p.Party != nil && p.PartyID == "" {
+		p.PartyID = p.Party.StringID()
+	}
+	if p.Party == nil && p.PartyID != "" {
+		p.Party = datastore.NewKey(ctx, personKind, p.PartyID, 0, nil)
+	}
+	if p.PlusOne != nil && p.PlusOneID == "" {
+		p.PlusOneID = p.PlusOne.StringID()
+	}
+	if p.PlusOne == nil && p.PlusOneID != "" {
+		p.PlusOne = datastore.NewKey(ctx, personKind, p.PlusOneID, 0, nil)
+	}
+	if p.IsPlusOneOf != nil && p.IsPlusOneOfID == "" {
+		p.IsPlusOneOfID = p.IsPlusOneOf.StringID()
+	}
+	if p.IsPlusOneOf == nil && p.IsPlusOneOfID != "" {
+		p.IsPlusOneOf = datastore.NewKey(ctx, personKind, p.IsPlusOneOfID, 0, nil)
+	}
+	if p.WillAccompany != nil && p.WillAccompanyID == "" {
+		p.WillAccompanyID = p.WillAccompany.StringID()
+	}
+	if p.WillAccompany == nil && p.WillAccompanyID != "" {
+		p.WillAccompany = datastore.NewKey(ctx, personKind, p.WillAccompanyID, 0, nil)
+	}
+	return p
+}
+
+func CreatePeople(ctx context.Context, people []Person) ([]Person, error) {
+	var keys []*datastore.Key
+	var err error
+	for pos, person := range people {
+		if person.ID == "" {
+			person.ID = uuid.NewID().String()
+		}
+		person = person.FillKeyIDs(ctx)
+		people[pos] = person
+		keys = append(keys, person.Key(ctx))
+	}
+	_, err = datastore.PutMulti(ctx, keys, people)
+	return people, err
+}
+
+func ListParties(ctx context.Context) ([]Party, error) {
+	q := datastore.NewQuery(partyKind).Order("Name")
+	result := q.Run(ctx)
+	var err error
+	var parties []Party
+	for err == nil {
+		var party Party
+		var key *datastore.Key
+		key, err = result.Next(&party)
+		if err == nil {
+			party.ID = key.StringID()
+			party = party.FillKeyIDs(ctx)
+			parties = append(parties, party)
+		}
+	}
+	if err == datastore.Done {
+		err = nil
+	}
+	return parties, err
+}
+
+func GetParties(ctx context.Context, ids []string) ([]Party, error) {
+	var keys []*datastore.Key
+	for _, id := range ids {
+		keys = append(keys, datastore.NewKey(ctx, partyKind, id, 0, nil))
+	}
+	parties := make([]Party, len(keys))
+	err := datastore.GetMulti(ctx, keys, parties)
+	if err != nil {
+		return []Party{}, err
+	}
+	for pos, party := range parties {
+		party.ID = keys[pos].StringID()
+		party = party.FillKeyIDs(ctx)
+		parties[pos] = party
+	}
+	return parties, nil
+}
+
+func GetPartyByMagicWord(ctx context.Context, word string) (Party, error) {
+	q := datastore.NewQuery(partyKind).Filter("MagicWord =", word).Limit(1)
+	var parties []Party
+	keys, err := q.GetAll(ctx, &parties)
+	if err != nil {
+		return Party{}, err
+	}
+	if len(parties) < 1 {
+		return Party{}, ErrMagicWordNotFound
+	}
+	party := parties[0]
+	party.ID = keys[0].StringID()
+	party = party.FillKeyIDs(ctx)
+	return party, nil
+}
+
+func ListPeople(ctx context.Context, party *datastore.Key) ([]Person, error) {
+	q := datastore.NewQuery("Person")
+	if party != nil {
+		q.Filter("Party =", party)
+	}
+	result := q.Run(ctx)
+	var err error
+	var people []Person
+	for err == nil {
+		var person Person
+		var key *datastore.Key
+		key, err = result.Next(&person)
+		if err == nil {
+			person.ID = key.StringID()
+			person = person.FillKeyIDs(ctx)
+			people = append(people, person)
+		}
+	}
+	if err == datastore.Done {
+		err = nil
+	}
+	return people, err
+}
+
+func GetPeople(ctx context.Context, ids []string) ([]Person, error) {
+	var keys []*datastore.Key
+	for _, id := range ids {
+		keys = append(keys, datastore.NewKey(ctx, personKind, id, 0, nil))
+	}
+	people := make([]Person, len(keys))
+	err := datastore.GetMulti(ctx, keys, people)
+	if err != nil {
+		return people, err
+	}
+	for pos, person := range people {
+		person.ID = keys[pos].StringID()
+		person = person.FillKeyIDs(ctx)
+		people[pos] = person
+	}
+	return people, nil
+}
---
+++ rsvp/rsvp.go
@@ -0,0 +1,130 @@
+package rsvp
+
+import (
+	"fmt"
+	"net/http"
+
+	"code.secondbit.org/trout.hg"
+	"darlinggo.co/api"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+)
+
+const scope = "https://www.googleapis.com/auth/userinfo.email"
+
+func init() {
+	var router trout.Router
+	router.Endpoint("/parties").Methods("PUT").Handler(api.CORSMiddleware(http.HandlerFunc(putPartiesHandler)))
+	router.Endpoint("/parties").Methods("GET").Handler(api.CORSMiddleware(http.HandlerFunc(getPartiesHandler)))
+	router.Endpoint("/people").Methods("PUT").Handler(api.CORSMiddleware(http.HandlerFunc(putPeopleHandler)))
+	router.Endpoint("/people").Methods("GET").Handler(api.CORSMiddleware(http.HandlerFunc(getPeopleHandler)))
+	http.Handle("/", router)
+}
+
+type Response struct {
+	Parties []Party            `json:"parties,omitempty"`
+	People  []Person           `json:"people,omitempty"`
+	Error   []api.RequestError `json:"errors,omitempty"`
+}
+
+func putPartiesHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	var partiesReq struct {
+		Parties []Party `json:"parties"`
+	}
+	err := api.Decode(r, &partiesReq)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+		return
+	}
+	// TODO(paddy): authenticate if it would create a new party
+	// TODO(paddy): require codeword to update existing party
+	parties, err := CreateParties(ctx, partiesReq.Parties)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+		return
+	}
+	api.Encode(w, r, http.StatusOK, Response{Parties: parties})
+}
+
+func putPeopleHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	var peopleReq struct {
+		People []Person `json:"people"`
+	}
+	err := api.Decode(r, &peopleReq)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+		return
+	}
+	// TODO(paddy): authenticate if it would create a new non-plus-one person
+	// TODO(paddy): require codeword to update existing people
+	people, err := CreatePeople(ctx, peopleReq.People)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+		return
+	}
+	api.Encode(w, r, http.StatusOK, Response{People: people})
+}
+
+func getPeopleHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	var people []Person
+	var err error
+	personIDs := r.URL.Query()["person_id"]
+	partyID := r.URL.Query().Get("party_id")
+	var party *datastore.Key
+	if partyID != "" {
+		party = datastore.NewKey(ctx, partyKind, partyID, 0, nil)
+	}
+	switch {
+	case len(personIDs) > 0:
+		// TODO(paddy): authenticate; use the codeWord and only allow retrieving people from that party
+		people, err = GetPeople(ctx, personIDs)
+		if err != nil {
+			fmt.Fprintf(w, "%+v\n", err)
+			return
+		}
+	default:
+		// TODO(paddy): authenticate
+		people, err = ListPeople(ctx, party)
+		if err != nil {
+			fmt.Fprintf(w, "%+v\n", err)
+			return
+		}
+	}
+	api.Encode(w, r, http.StatusOK, Response{People: people})
+}
+
+func getPartiesHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	var parties []Party
+	var err error
+	partyIDs := r.URL.Query()["party_id"]
+	partyWord := r.URL.Query().Get("magic_word")
+	switch {
+	case len(partyIDs) > 0:
+		// TODO(paddy): authenticate
+		parties, err = GetParties(ctx, partyIDs)
+		if err != nil {
+			fmt.Fprintf(w, "%+v\n", err)
+			return
+		}
+	case partyWord != "":
+		party, err := GetPartyByMagicWord(ctx, partyWord)
+		if err != nil {
+			fmt.Fprintf(w, "%+v\n", err)
+			return
+		}
+		parties = append(parties, party)
+	default:
+		// TODO(paddy): authenticate
+		parties, err = ListParties(ctx)
+		if err != nil {
+			fmt.Fprintf(w, "%+v\n", err)
+			return
+		}
+	}
+	api.Encode(w, r, http.StatusOK, Response{Parties: parties})
+}
